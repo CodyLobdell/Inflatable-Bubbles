cb3e09f1d626bdf37d3e972a6e352c96856e110f
Cody Lobdell <thecreator85@gmail.com>
2023-10-01 19:47:07 -0700

    added effect for relection

--- script.js
+++ script.js
@@ -15,10 +15,24 @@ class Particle {
     this.effect = effect;
     this.radius = Math.floor(Math.random() * 8 + 8);
     this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2);
-    this.vx = Math.random() * 0.2 - 0.5;
-    this.vy = Math.random() * 1 - 0.5;
-    this.pushX = 0;
-    this.pushY = 0;
-    this.friction = 0.95;
+    this.vx = Math.random() * 0.2 - 0.1;
+    this.vy = Math.random() * 0.2 - 0.1;
+  }
+  draw(context){
+    context.fillStyle = gradient;
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.fill();
+    context.stroke();
+
+    context.fillStyle = 'white';
+    context.beginPath();
+    context.arc(this.x - this.radius * 0.2, this.y - this.radius * 0.3, this.radius * 0.6, 0, Math.PI * 2);
+    context.fill();
+  }
+  update(){
+    if (this.effect.mouse.pressed){
+      const dx = this.x - this.effect.mouse.x;
+    }
   }
 }
